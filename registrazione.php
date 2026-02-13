204929686b4dd093e81088e2ccf9699dd80a83ba
Danilo-Volonte <volonte.danilo.06@itisdalmine.edu.it>
2026-02-13 19:40:08 +0100

    iscrizione da risolvere forse

---
+++ registrazione.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="it">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>REGISTRAZIONE</title>
+</head>
+<body>
+        <form action="aggiuntaUtente.php" method="POST">
+        <p> per registrarti</p>
+        <p> inserisic la tua email</p>
+        <input type="text" name="email" placeholder="inserisci la tua email">
+
+
+        <p> inserisic la tua password </p>
+        <input type="text" name="password" placeholder="inserisci la tua email">
+
+        <button type="submit">invia</button>
+    
+</body>
+</html>
